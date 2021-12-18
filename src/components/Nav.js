76456dc9ab6c4056564fe0abeff399a0bd1308f7
Makiyu-py <73825066+Makiyu-py@users.noreply.github.com>
2021-12-19 00:11:34 +0800

    converted anchor tags on nav to a mapped var to make it DRY

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,19 @@
 export default function NavBar() {
+	// capitalize not working on second anchor tag so
+	const capitalize = (s) => s && s[0].toUpperCase() + s.slice(1);
+
+	let redirects = ['games', 'books'].map((i) => (
+		<a
+			href={`#${i}`}
+			className="px-3 py-2 rounded-md text-sm font-medium underline hover:bg-gray-900"
+		>
+			{capitalize(i)}
+		</a>
+	));
 	return (
 		<div className="sticky top-0 text-slate-100 p-5 w-full bg-gray-800 border-b border-slate-700">
 			Christmas List 2021
-			<div className="float-right mr-2 underline">
-				<a
-					href="#games"
-					className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-900"
-				>
-					Games
-				</a>
-				<a
-					href="#books"
-					className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-900"
-				>
-					Books
-				</a>
-			</div>
+			<div className="float-right mr-2">{redirects}</div>
 		</div>
 	);
 }
