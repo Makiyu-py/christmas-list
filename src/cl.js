12e387875327356ab07099c013d41728b7eee576
Makiyu-py <73825066+Makiyu-py@users.noreply.github.com>
2021-12-27 23:30:59 +0800

    add `gotten` on items

--- src/cl.js
+++ src/cl.js
@@ -5,21 +5,25 @@ export let games = [
 		caption: 'Hollow Knight',
 		link: 'https://www.nintendo.com/games/detail/hollow-knight-switch/',
 		src: 'https://m.media-amazon.com/images/M/MV5BYTljYjM2OGYtNzU5NC00ODBiLTk4Y2ItMmE0ZDUxMzJmYWU4XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg',
+		gotten: false,
 	},
 	{
 		caption: "Death's Door",
 		link: 'https://www.nintendo.com/games/detail/deaths-door-switch/',
 		src: 'https://storage.oyungezer.com.tr/ogz-public/public/game/855/cover_60fd29e3b6e08_855.jpg',
+		gotten: false,
 	},
 	{
 		caption: `Kena: Bridge of Spirits (Deluxe Edition)`,
 		link: 'https://ecommerce.datablitz.com.ph/products/ps4-kena-bridge-of-spirit-deluxe-edition',
 		src: 'https://image.api.playstation.com/vulcan/ap/rnd/202102/2307/5DwFII1MYsBn48HjmvUu0TEd.jpg',
+		gotten: false,
 	},
 	{
 		caption: 'Celeste',
 		link: 'https://www.nintendo.com/games/detail/celeste-switch/',
 		src: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Celeste_box_art_final.png',
+		gotten: false,
 	},
 ];
 
@@ -28,11 +32,12 @@ export let books = [
 		caption: 'They Both Die at the End',
 		link: 'https://www.goodreads.com/book/show/33385229-they-both-die-at-the-end',
 		src: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1494333138l/33385229._SY475_.jpg',
+		gotten: true,
 	},
 	{
 		caption: 'A Deadly Education',
 		link: 'https://www.goodreads.com/book/show/50548197-a-deadly-education',
 		src: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1596909044i/50548197.jpg',
+		gotten: false,
 	},
 ];
-
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,19 @@
-export default function Item({ src, caption, link }) {
+export default function Item({ src, caption, link, gotten }) {
+	let figClassGottenExtra = !gotten ? " hover:underline" : ""
+	
+	let figcaptionClasses = ["mt-1", "text-center"]
+	if (gotten) figcaptionClasses.push("text-zinc-400", "line-through")
+	else figcaptionClasses.push("text-zinc-100")
+
 	return (
-		<figure className="w-[min-content] m-0 hover:text-zinc-200 hover:underline">
+		<figure className={"w-[min-content] m-0 hover:text-zinc-200" + figClassGottenExtra}>
 			<a href={link} target="_blank" rel="noreferrer">
 				<img
 					className="max-w-[100vw] h-[200px]"
 					src={src}
 					alt={caption}
 				></img>
-				<figcaption className="mt-1 text-center text-zinc-100">
+				<figcaption className={figcaptionClasses.join(' ')}>
 					{caption}
 				</figcaption>
 			</a>
